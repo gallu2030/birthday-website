2f9ac5d4951043b2ea75b3c05a7149b73cfff81d
gallu2030 <gallu4206@gmail.com>
2026-07-21 20:14:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,34 +4,33 @@ const btn = document.getElementById("playBtn");
 const slider = document.getElementById("slider");
 
 const photos = [
-"birthday.jpg",
-"photo1.jpg",
-"photo2.jpg",
-"photo3.jpg"
+  "birthday.jpg",
+  "photo1.jpg",
+  "photo2.jpg",
+  "photo3.jpg"
 ];
 
 let i = 0;
 
 btn.onclick = () => {
-music.play();
   music.currentTime = 0;
-music.play();
+  music.play();
 
-setTimeout(() => {
-  music.pause();
-  music.currentTime = 0;
-}, 15000);
+  setTimeout(() => {
+    music.pause();
+    music.currentTime = 0;
+  }, 12000);
 
-alert("🎉 Happy Birthday! Wishing you endless happiness ❤️");
+  alert("🎉 Happy Birthday! Wishing you endless happiness ❤️");
 
-setInterval(() => {
-i = (i + 1) % photos.length;
-slider.src = photos[i];
-}, 2500);
+  setInterval(() => {
+    i = (i + 1) % photos.length;
+    slider.src = photos[i];
+  }, 2500);
 
-btn.innerHTML = "❤️ Enjoy Your Surprise ❤️";
+  btn.innerHTML = "❤️ Enjoy Your Surprise ❤️";
 };
 
 window.onload = () => {
-document.body.style.opacity = "1";
+  document.body.style.opacity = "1";
 };
