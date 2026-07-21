f3f50eb5404409850b3bb1cc2e39dfcfa3385ee5
gallu2030 <gallu4206@gmail.com>
2026-07-21 19:51:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 // Birthday Website Scri
 const music = document.getElementById("music");
 const btn = document.getElementById("playBtn");
+const slider = document.getElementById("slider");
 
-btn.addEventListener("click", () => {
-  music.play();
-  alert("🎉 Happy Birthday! ❤️");
-  btn.innerHTML = "🎵 Playing...";
-});
+const photos = [
+"birthday.jpg",
+"photo1.jpg",
+"photo2.jpg",
+"photo3.jpg"
+];
+
+let i = 0;
+
+btn.onclick = () => {
+music.play();
+
+alert("🎉 Happy Birthday! Wishing you endless happiness ❤️");
+
+setInterval(() => {
+i = (i + 1) % photos.length;
+slider.src = photos[i];
+}, 2500);
+
+btn.innerHTML = "❤️ Enjoy Your Surprise ❤️";
+};
 
 window.onload = () => {
-  document.body.classList.add("loaded");
+document.body.style.opacity = "1";
 };
